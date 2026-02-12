d61c375343e8fb427a6bf01afdb113dfe08ac824
Ruth Thomas <ruthathomas026@gmail.com>
2026-02-11 22:07:33 -0700

    remade startupVite folder :')

---
+++ startupVite/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+export default function App() {
+  return <div className="body bg-dark text-light">sob</div>;
+}
