acc6c0f1ed9fd5558b287317f9cbe2cc475cbcb1
Ruth Thomas <ruthathomas026@gmail.com>
2026-02-28 12:30:29 -0700

    made it so you can navigate to home from login page button

--- startupVite/src/login/authenticated.jsx
+++ startupVite/src/login/authenticated.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from 'react-router-dom';
+
 export function Authenticated(props) {
+
+    const navigate = useNavigate();
 
     // logout function
     function logout() {
@@ -10,7 +14,7 @@ export function Authenticated(props) {
         <div className="test">
             <p style={{textAlign: 'center', fontWeight: 'bold'}}>hey, {props.username} :)</p>
             <div>
-                <button style={{marginRight: 0.5 + "rem"}} onClick={() => logout()}>home</button>
+                <button style={{marginRight: 0.5 + "rem"}} onClick={() => navigate('/home')}>home</button>
                 <button style={{marginLeft: 0.5 + "rem"}} onClick={() => logout()}>logout</button>
             </div>
         </div>
